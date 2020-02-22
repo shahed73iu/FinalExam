9f0d22af6a2556669fcf71eefb0e47ad161ab642
shahed73iu <shahedimamuddin@gmail.com>
2020-02-23 00:48:43 +0600

    APi Project

---
+++ FinalProjectWebApi/FinalProjectWebApi/Controllers/TokenController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FinalProjectWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        public readonly IConfiguration _configuration;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TokenController(IConfiguration config,
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _configuration = config;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+       
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string username, string password)
+        {
+            if (username != null && password != null)
+            {
+                var user = await _userManager.FindByEmailAsync(username);
+                var result = await _signInManager.CheckPasswordSignInAsync(user, password, true);
+                //var user = "shahedimamuddin@gmail.com";
+                //var result = "Shahed@1234";
+
+                if (result != null && result.Succeeded)
+                {
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = new ClaimsIdentity(new Claim[]
+                        {
+                            new Claim(ClaimTypes.Name, user.Id.ToString())
+                        }),
+                        Expires = DateTime.UtcNow.AddDays(7),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                    };
+                    var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                    return Ok(tokenHandler.WriteToken(token));
+                }
+                else
+                {
+                    return BadRequest("Invalid credentials");
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
